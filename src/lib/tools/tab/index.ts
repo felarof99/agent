6300bb207188bc488d07747eed4f1ff50f88fe2e
Felarof <nithin.sonti@gmail.com>
2025-07-28 16:01:45 -0700

    GroupTabs tool ready

--- src/lib/tools/tab/index.ts
+++ src/lib/tools/tab/index.ts
@@ -1,2 +1,4 @@
 export { TabOperationsTool, createTabOperationsTool, TabOperationInputSchema } from './TabOperationsTool'
 export type { TabOperationInput } from './TabOperationsTool'
+export { GroupTabsTool, createGroupTabsTool, GroupTabsInputSchema } from './GroupTabsTool'
+export type { GroupTabsInput } from './GroupTabsTool'
